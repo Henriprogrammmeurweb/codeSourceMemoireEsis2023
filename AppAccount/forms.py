8d5869e6bb8d7338b9bf534d89641d28bb85ca22
Henriprogrammeurweb <comptedepotgithub@gmail.com>
2023-10-08 12:25:29 +0200

    Changement des messages sur chaque formulaire de reinitialisation de mot de passe

--- AppAccount/forms.py
+++ AppAccount/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from django.contrib.auth.forms import PasswordChangeForm, PasswordResetForm, SetPasswordForm
+from django.contrib.auth.forms import (PasswordChangeForm,
+                                       PasswordResetForm,
+                                       SetPasswordForm)
 
 
 class FormLogin(forms.Form):
