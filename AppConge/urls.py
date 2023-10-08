f10fe1d4575ab55da7cc229b7d52b760d3f0e65c
Henriprogrammeurweb <comptedepotgithub@gmail.com>
2023-10-08 13:43:08 +0200

    Changement des urls de mod et supp dans AppConge et Planning en le nommant de la manière que les fonctions elles pour restreindre les accès

--- AppConge/urls.py
+++ AppConge/urls.py
@@ -5,9 +5,9 @@ from .import views
 urlpatterns=[
     path('conge-user', views.CongeUser, name="CongeUser"),
     path('ajout-conge', views.ajoutConge, name="ajoutConge"),
-    path('detail/<str:id>/conge', views.detailConge,name="detailConge"),
-    path('modif/<str:id>/conge', views.modifConge, name="modifConge"),
-    path('supp/<str:id>/conge', views.suppConge, name='suppConge'),
+    path('detail/<int:id>/Conge', views.detailConge,name="detailConge"),
+    path('modif/<int:id>/Conge', views.modifConge, name="modifConge"),
+    path('supp/<int:id>/Conge', views.suppConge, name='suppConge'),
     path('conge-attente', views.congeAttente, name='congeAttente'),
     path('approuve-rejet-conge', views.approuveRejetConge, name="approuveRejetConge"),
     path('confirm-retour-conge', views.confirmRetourConge,name="confirmRetourConge"),
@@ -16,9 +16,9 @@ urlpatterns=[
     path('conge-encours', views.congeEncours, name='congeEncours'),
     path('conge-attente-user', views.congeAttenteUser, name="congeAttenteUser"),
     path('liste-approbation-rejet', views.listeApprobationRejet, name="listeApprobationRejet"),
-    path('modif-approbation/<str:id>/rejet', views.modifApprobationRejet, name='modifApprobationRejet'),
-    path('supp/<str:id>/demande', views.suppDemande, name="suppDemande"),
-    path('detail/<str:id>/demande', views.detailDemande, name="detailDemande"),
+    path('modifApprobation/<str:id>/Rejet', views.modifApprobationRejet, name='modifApprobationRejet'),
+    path('supp/<int:id>/Demande', views.suppDemande, name="suppDemande"),
+    path('detail/<int:id>/Demande', views.detailDemande, name="detailDemande"),
     path('consulter-conge-encours', views.consulterCongeEncours, name="consulterCongeEncours"),
     path('send-email', views.sendEmail, name='sendEmail')
 ]
--- AppPlanning/urls.py
+++ AppPlanning/urls.py
@@ -5,11 +5,11 @@ from .import views
 urlpatterns=[
     path("liste-planning", views.listePlanning, name="listePlanning"),
     path('ajout-planning', views.ajoutPlanning,name="ajoutPlanning"),
-    path('modif/<str:id>/planning', views.modifPlanning, name="modifPlanning"),
-    path('supp/<str:id>/planning', views.suppPlanning,name="suppPlanning"),
+    path('modif/<str:id>/Planning', views.modifPlanning, name="modifPlanning"),
+    path('supp/<str:id>/Planning', views.suppPlanning,name="suppPlanning"),
     path('liste-annee', views.listeAnnee, name='listeAnnee'),
     path('ajout-annee', views.ajoutAnnee, name="ajoutAnnee"),
-    path('modif/<str:id>/annee', views.modifAnnee, name="modifAnnee"),
-    path('supp/<str:id>/annee', views.suppAnnee, name='suppAnnee'),
-    path('planning/<str:id>/annee', views.planningAnnee, name='planningAnnee')
+    path('modif/<str:id>/Annee', views.modifAnnee, name="modifAnnee"),
+    path('supp/<str:id>/Annee', views.suppAnnee, name='suppAnnee'),
+    path('planning/<str:id>/Annee', views.planningAnnee, name='planningAnnee')
 ]
