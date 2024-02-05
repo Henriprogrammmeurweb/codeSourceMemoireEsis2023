e65f00504ed22a9b6e6cf19a3f95a16b515ea27e
KIUKA HENRI ALIAS HENRI PROGRAMMEUR WEB <comptedepotgithub@gmail.com>
2024-02-05 18:01:41 +0100

    Mise en jour de la page d'accueil du site'

--- AppConge/tests.py
+++ AppConge/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
+from .import models
+from AppAccount.models import Personnel
+import datetime
 
-# Create your tests here.
+
+
+
+class CreateCongeTestCate(TestCase):
+    def setUp(self):
+        self.personnel={
+            'username':'KIUKA',
+            'postnom':'MBAYO',
+            'prenom':'HENRI',
+            'sexe':'M',
+            'etat_civil':'M'
+        }
+        self.date_debut='02-02-2024'
+        self.date_fin='02-03-2024'
+        self.convert_date_debut=datetime.datetime.strptime(self.date_debut, '%d-%m-%Y').date()
+        self.convert_date_fin=datetime.datetime.strptime(self.date_fin, '%d-%m-%Y').date()
+     
+        self.getPersonnel=Personnel.objects.create(**self.personnel)
+        self.conge=models.Conge.objects.create(personnel=self.getPersonnel,titre='Voyage',
+                                               nature='cumulé',
+                                               motif="Voir ma famille", 
+                                               date_debut=self.convert_date_debut,
+                                               date_fin=self.convert_date_fin)
+        
+        self.reponse=models.Demande.objects.create(conge=self.conge, commentaire="Oui",approbation=False)
+        
+    # def test_ok(self):
+    #     self.assertNotIn(self.conge, self.conge.get_conge_demander)
+
+    
+    def test_comment(self):
+        self.assertEqual(self.conge.getReponseConge, '0 jour')
+        
+        
+
+        
+        
+
+        
+
+
+
